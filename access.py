9df4af3559978529251c72a55e0d4dd947ebd20f
Kai <riselikethemoon@gmail.com>
2016-02-01 14:54:30 -0800

    set up problem space

---
+++ access.py
@@ -0,0 +1,19 @@
+"""
+Given list of codes consisting of chars a-z, determine number of unique codes.
+Codes can be read forward or backward, so abc == cba.
+"""
+
+def answer(x):
+    """
+    Given list of codes consisting of chars a-z, determine number of unique codes.
+    Codes can be read forward or backward, so abc == cba.
+    """
+
+    unique_codes = set()
+
+    for code in x:
+        print code
+
+    return "apple"
+
+print answer(["abc", "def", "fed"])
