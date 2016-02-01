58ed2734157f546a7a31abbe3771fb72907f923d
Kai <riselikethemoon@gmail.com>
2016-02-01 15:55:45 -0800

    debug notes in

--- zombit.py
+++ zombit.py
@@ -1,15 +1,29 @@
 def answer(intervals):
     """Given a list of intervals, return the total number of supervised hours"""
 
-    supervised_hours = []
+    supervised_hours = intervals[0]
 
-    for interval in intervals:
-        for num in range(interval[0], interval[1]):
-            if num not in supervised_hours:
-                supervised_hours.append(num)
+    # for interval in intervals:
+    #     for num in range(interval[0], interval[1]):
+    #         if num not in supervised_hours:
+    #             supervised_hours.append(num)
 
-    return len(supervised_hours)
+    # return len(supervised_hours)
 
+
+
+    for interval in intervals[1:]:
+        print "interval:", interval
+    #     for hour_group in supervised_hours:
+    #         print hour_group
+    #         if (interval[0] in range(hour_group[0], hour_group[1])) and (interval[1] > hour_group[1]):
+    #             print "interval in range"
+    #             hour_group[1] = interval[1]
+    #         else:
+    #             "adding interval"
+    #             supervised_hours.append(interval)
+
+    print "suped:", supervised_hours
 
 
 # set gives memory errors, lists give time errors.
@@ -20,4 +34,4 @@ print answer([[1, 3], [3, 6]])
 
 # intervals = [[10, 14], [4, 18], [19, 20], [19, 20], [13, 20]]
 # > 16
-print answer([[10, 14], [4, 18], [19, 20], [19, 20], [13, 20]])
+# print answer([[10, 14], [4, 18], [19, 20], [19, 20], [13, 20]])
